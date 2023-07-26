ae9f22e5d2aa0682ac4efff527801186241a689a
Ayush Sisodia <64357307+ayushsisodia7@users.noreply.github.com>
2023-07-26 14:21:50 +0530

    Add files

--- Bitcoin_API_program/index.js
+++ Bitcoin_API_program/index.js
@@ -1,1 +1,38 @@
+const express=require("express")
+const BodyParser=require("body-parser")
+const request=require("request")
 
+var app=express();
+app.use(BodyParser.urlencoded({extended:true}));
+
+app.get('/',function(req,res){   //include html file for webpage
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post('/',function(req,res){   //output after entering data
+    var crypto=req.body.crypto;
+    var fiat=req.body.fiat;
+    var amount=req.body.amount;
+
+    var options={    //Setting url with parameters
+        url:"https://apiv2.bitcoinaverage.com/convert/global",
+        method:"GET",
+        qs:{
+            from:crypto,
+            to:fiat,
+            amount:amount
+        }
+    }
+
+    request(options,function(error,response,body){    //call to API
+    var data=JSON.parse(body);
+    var p=data.price;
+    res.write("The current price of "+crypto+" is "+p);
+    res.send();
+    })
+    
+})
+
+app.listen(3000,function(){   //port server for localhost
+    console.log("Server is running");
+})
